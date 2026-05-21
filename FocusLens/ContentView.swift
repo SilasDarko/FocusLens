7023907c07b93311e69babb7ef66b4a2a84f6bb2
Silas Offei Darko <silasdarko2023@gmail.com>
2026-05-21 16:22:54 -0500

    Initial Commit

---
+++ FocusLens/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FocusLens
+//
+//  Created by Silas Offei Darko on 5/21/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FocusLens/FocusLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusLensApp.swift
+//  FocusLens
+//
+//  Created by Silas Offei Darko on 5/21/26.
+//
+
+import SwiftUI
+
+@main
+struct FocusLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
